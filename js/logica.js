b98c91a5cd1cda6fc5b290896030633c6127f942
Frank <frank_toledo_009@hotmail.com>
2017-08-14 19:19:32 -0300

    logica
    detectar al obispo

---
+++ js/logica.js
@@ -0,0 +1,9 @@
+function obispoDetect(){
+  var elm = document.getElementById("obispoActual").offsetTop;
+  var pantalla = document.getElementById("main").scrollTop;
+
+  if(elm-200 < pantalla){
+    
+  }
+
+}
