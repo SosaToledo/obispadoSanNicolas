8ec7da1ab5f7410ddc18f4f3a055da66b1df5c0d
Luciano <luchososa1990@hotmail.com.ar>
2017-08-17 20:16:39 -0300

    de todo

--- js/logica.js
+++ js/logica.js
@@ -3,7 +3,16 @@ function obispoDetect(){
   var pantalla = document.getElementById("main").scrollTop;
 
   if(elm-200 < pantalla){
-    
+
   }
 
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
