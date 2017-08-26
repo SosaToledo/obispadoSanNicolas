367b79934538d06f17a5dd1d84f70e73f746d249
Luciano <luchososa1990@hotmail.com.ar>
2017-08-26 18:42:23 -0300

    Presentación

--- js/logica.js
+++ js/logica.js
@@ -9,7 +9,7 @@ function detect(){
       }
   }
   var nav = document.getElementById("barra").style;
-  if ((pantalla>200 && ancho>500) || (pantalla>500 && ancho < 500)){
+  if ((pantalla>200 && ancho>500) || (ancho < 500)){
     nav.position = "fixed";
     nav.top = "0";
   }else{
