353ca3d9cde249f88b38f3cf59dcafd9411a88c9
Unknown <frank_toledo_009@hotmail.com>
2017-08-28 01:59:49 -0300

    En armonia con el hosting

---
+++ header.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Obispado de San Nicolas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
+    <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/estiloMedia.css">
+  </head>
+  <!--  -->
+  <body id="main" onscroll="detect()">
+
+    <header style="background: url('<?php bloginfo("template_url") ?>/img/portada.jpg') no-repeat center center; background-size:cover;">
+      <p>Obispado de San Nicolas de los Arroyos</p>
+    </header>
+    <nav id="barra">
+        <div class="dropdown">
+          <a href="#">Inicio</a>
+        </div>
+        <div class="dropdown">
+          <a href="#">Obispo</a>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <a href="#">Catedral</a>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <a href="#">Virgen del Rosario</a>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <a href="#">Crecensia</a>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <a href="#">Curia</a>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <a href="#">Colegios Religiosos</a>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <a href="#">Noticias</a>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+        <img style="width:32px; height: 32px; margin:3px;"src="<?php bloginfo('template_url') ?>/img/iconos/facebook.jpg" alt="Facebook">
+      </nav>
---
+++ index.php
@@ -0,0 +1,189 @@
+<?php get_header(); ?>
+  <!--  -->
+<div class="contendorData">
+  <section id="sliderPrincipal" class="slider">
+    <div class="tope_slider"></div>
+    <div class="sliderPrincipal" style="display:block; background:url(<?php bloginfo('template_url') ?>/img/iglesias-en-venecia.jpg) center no-repeat;">
+      <div class="picText">
+        <h1>Imagen Uno</h1>
+      </div>
+    </div>
+    <div class="sliderPrincipal" style="display:block; background:url(img/portada.jpg) center no-repeat;">
+      <div class="picText">
+        <h1>Imagen Dos</h1>
+      </div>
+    </div>
+    <div class="sliderPrincipal" style="display:block; background:url(img/iglesias-en-venecia.jpg) center no-repeat;">
+      <div class="picText">
+        <h1>Imagen Tres</h1>
+      </div>
+    </div>
+    <div class="sliderPrincipal" style="display:block; background:url(img/iglesias-en-venecia.jpg) center no-repeat;">
+      <div class="picText">
+        <h1>Imagen Cuatro</h1>
+      </div>
+    </div>
+
+    <div class="botonSliderIzquirda" style="cursor:pointer" onclick="plusDivs(-1)">&#10094;&#10094;</div>
+    <div class="botonSliderDerecha" style="cursor:pointer" onclick="plusDivs(1)">&#10095;&#10095;</div>
+
+    <div class="circulos" style="text-align:center">
+      <div class="cirDiv">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+        <span class="dot" onclick="currentSlide(4)"></span>
+      </div>
+    </div>
+  </section>
+  <section id="" class="nuestraDiocesis">
+    <div class="" style="left:10%;height:25px;;background:url(<?php bloginfo('template_url') ?>/img/iconos/separador.png) no-repeat;width:100%; position:relative;">
+      <p style="text-align:left">Nuestra diócesis</p>
+    </div>
+    <div class="diocesisIconos">
+      <img src="<?php bloginfo('template_url') ?>/img/iconos/geoposicion.png" alt="">
+      <p>Historia</p>
+    </div>
+    <div class="diocesisIconos">
+      <img src="<?php bloginfo('template_url') ?>/img/iconos/geoposicion.png" alt="">
+      <p>Mapa</p>
+    </div>
+    <div class="diocesisIconos">
+      <img src="<?php bloginfo('template_url') ?>/img/iconos/curia.png" alt="">
+      <p>Curia</p>
+    </div>
+    <div class="diocesisIconos">
+      <img src="<?php bloginfo('template_url') ?>/img/iconos/AGENDA.png" alt="">
+      <p>Calendario <br> Diocesano</p>
+    </div>
+  </section>
+
+  <div class="separadorInvertido" style="width:100%;">
+    <img src="<?php bloginfo('template_url') ?>/img/iconos/DIV3.png" alt="">
+  </div>
+
+  <section id="obispoActual" class="obispoActual">
+    <article id="obispo">
+      <div class="obispoInfoIzq">
+        <div class="info">
+          <a href="#"><img src="<?php bloginfo('template_url') ?>/img/iconos/ESCUDO.png" alt=""></a>
+          <p>Escudo</p>
+        </div>
+        <div class="info">
+          <a href="#"><img src="<?php bloginfo('template_url') ?>/img/iconos/ESCRITOS.png" alt=""></a>
+          <p>Escritos</p>
+        </div>
+        <div class="info">
+          <a href="#"><img src="<?php bloginfo('template_url') ?>/img/iconos/BIOGRAFIA.png" alt=""></a>
+          <p>Biografia</p>
+        </div>
+        <div class="info">
+          <a href="#"><img src="<?php bloginfo('template_url') ?>/img/iconos/FOTOGRAFIAS.png" alt=""></a>
+          <p>Fotografia</p>
+        </div>
+      </div>
+      <div class="obispoImg">
+        <img src="<?php bloginfo('template_url') ?>/img/obispoActual.jpg" alt="">
+        <p></p>
+      </div>
+      <div class="obispoInfoDer">
+        <div class="info">
+          <img src="<?php bloginfo('template_url') ?>/img/iconos/ULTIMOSESCRITOS.png" alt="">
+          <p>Ultimos escritos</p>
+        </div>
+        <div class="info">
+          <img src="<?php bloginfo('template_url') ?>/img/iconos/VIDEOS.png" alt="">
+          <p>Videos</p>
+        </div>
+        <div class="info">
+          <img src="<?php bloginfo('template_url') ?>/img/iconos/HOMILIAS.png" alt="">
+          <p>Homilias</p>
+        </div>
+      </div>
+    </article>
+    <aside class="">
+
+    </aside>
+  </section>
+  <section id="noticias" class="noticias">
+    <div class="separadorInvertido" style="width:100%;">
+      <img src="<?php bloginfo('template_url') ?>/img/iconos/DIV3.png" alt="">
+    </div>
+    <article class="noticiasPrincipales">
+      <div class="noticiasDiv">
+        <img src="<?php bloginfo('template_url') ?>/img/igleNoticias.jpg" alt="" style="border-radius: 50%;">
+        Peregrinaciones
+      </div>
+      <div class="noticiasDiv">
+        <img src="<?php bloginfo('template_url') ?>/img/igleNoticias.jpg" alt="" style="border-radius: 50%;">
+        Nombramientos
+      </div>
+      <div class="noticiasDiv">
+        <img src="<?php bloginfo('template_url') ?>/img/igleNoticias.jpg" alt="" style="border-radius: 50%;">
+        Retiros
+      </div>
+    </article>
+    <div class="separador" style="width:100%;">
+      <img src="<?php bloginfo('template_url') ?>/img/iconos/DIV3.png" alt="">
+    </div>
+    <article class="noticiasMultimedia">
+      <div class="noticiasDiv">
+        <a href="#">
+          <img src="<?php bloginfo('template_url') ?>/img/camaraIcono.jpg" alt="">
+        </a>
+        Fotos
+      </div>
+      <div class="noticiasDiv">
+        <img src="<?php bloginfo('template_url') ?>/img/audioIcono.jpg" alt="">
+        Audios
+      </div>
+      <div class="noticiasDiv">
+        <img src="<?php bloginfo('template_url') ?>/img/videoIcono.jpg" alt="">
+        Videos
+      </div>
+    </article>
+    <article class="noticiasSlider">
+      <h1>Proyectos</h1>
+    </article>
+  <footer id="ubicacion">
+    <h1>Obispado de San Nicolas</h1>
+    <p>Bartolomé Mitre 84 <br>
+    San Nicolas, Buenos Aires <br>
+    Tel y fax: (0336) 4422364 / 4429529</p>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26667.7039472087!2d-60.23595152441922!3d-33.332852032696024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x62183e69c44221af!2sIglesia+Adventista+San+Nicol%C3%A1s+Centro!5e0!3m2!1ses-419!2sar!4v1501950679251" width="900" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </footer>
+</div>
+
+<div class="separador" style="width:100%;">
+  <img src="<?php bloginfo('template_url') ?>/img/iconos/DIV3.png" alt="">
+</div>
+
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function currentSlide(n){
+  showDivs(slideIndex = n);
+}
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("sliderPrincipal");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].className = "sliderPrincipal";
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+  x[slideIndex-1].className += " animacionSlider";
+}
+</script>
+<script src="<?php bloginfo('template_url') ?>/js/logica.js"></script>
+</body>
+
+<?php get_footer(); ?>
