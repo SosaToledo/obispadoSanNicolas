1d0c26ba95f7a420a4889221caca9da82c420120
Unknown <frank_toledo_009@hotmail.com>
2017-09-13 05:03:32 -0300

    afinadas algunas cosas

--- obisposAnteriores.php
+++ obisposAnteriores.php
@@ -6,17 +6,28 @@ get_header();
 include "menu.php";
 ?>
 <img class="imgLupa" src="<?php bloginfo('template_url')?>/img/iconos/lupa.png">
-<h1 style="width:75%; margin:auto;">Obispos Anteriores</h1>
+<div class="tituloConBarra">
+	<h1>Obispos Anteriores</h1>
+	<img class="barraTitulos" src="<?php bloginfo('template_url'); ?>/img/barraObisposAnteriores.png" alt="">
+</div>
 <?php
-	query_posts('cat=23'); // Para excluír se usa el símbolo menos
+	// ObisposAnteriores id 34
+	// Emeritos id 29           -Frank
+	// ObisposAnteriores id 6
+	// Emeritos id 7            -web
+	query_posts('cat=6,7');
 		if ( have_posts() ) {
 			while ( have_posts() ) {
 				the_post();?>
 				<div class="contenedorObispos">
-					<?php if(has_post_thumbnail()){
+					<a href="<?php the_permalink();?>">
+						<?php if(has_post_thumbnail()){
 		        the_post_thumbnail('imgObispo');
-		      }?>
-					<h2 class="tituloPageObispos"><?php the_title() ?></h2>
+					} else {
+						?><img src="<?php bloginfo('template_url');?>/img/avatarGenerico.png"/><?php
+					}?>
+					</a>
+					<a href="<?php the_permalink();?>"><h2 class="tituloPageObispos"><?php the_title() ?></h2></a>
 					<p><?php the_excerpt();?></p>
 					<a href="<?php the_permalink();?>">+ Info</a>
 				</div>
