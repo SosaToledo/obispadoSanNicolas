a77054d364bd40eba5ba8ccf99e54cc2f90ba0c2
Luciano <luchososa1990@hotmail.com.ar>
2017-10-21 15:08:00 -0300

    chau animación

--- index.php
+++ index.php
@@ -276,27 +276,27 @@ include "menu.php";
 
 <script type="text/javascript">
 
-function detect(){
-  var ancho = screen.width;
-  var pantalla = document.getElementById("main").scrollTop;
-  var nav = document.getElementById("barra").style;
-  if ((pantalla>360 && ancho>500) || (ancho < 500)){
-    nav.position = "fixed";
-    nav.top = "0";
-  }else{
-    nav.position = "";
-    nav.top = "";
-  }
-  var elm = document.getElementById("obispoActual").offsetTop;
-  if(elm-500 < pantalla && ancho>500){
-      document.getElementById("IA").className = "info animacionIA";
-      document.getElementById("IC").className = "info animacionIC";
-      document.getElementById("IB").className = "info animacionIB";
-      document.getElementById("DA").className = "info animacionDA";
-      document.getElementById("DC").className = "info animacionDC";
-      document.getElementById("DB").className = "info animacionDB";
-  }
-
-}
+// function detect(){
+//   var ancho = screen.width;
+//   var pantalla = document.getElementById("main").scrollTop;
+//   var nav = document.getElementById("barra").style;
+//   if ((pantalla>360 && ancho>500) || (ancho < 500)){
+//     nav.position = "fixed";
+//     nav.top = "0";
+//   }else{
+//     nav.position = "";
+//     nav.top = "";
+//   }
+//   var elm = document.getElementById("obispoActual").offsetTop;
+//   if(elm-500 < pantalla && ancho>500){
+//       document.getElementById("IA").className = "info animacionIA";
+//       document.getElementById("IC").className = "info animacionIC";
+//       document.getElementById("IB").className = "info animacionIB";
+//       document.getElementById("DA").className = "info animacionDA";
+//       document.getElementById("DC").className = "info animacionDC";
+//       document.getElementById("DB").className = "info animacionDB";
+//   }
+//
+// }
 </script>
 <?php get_footer(); ?>
