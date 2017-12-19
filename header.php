ff5b25e613a05a384967ca58e1a62e3cb8239918
Unknown <frank_toledo_009@hotmail.com>
2017-12-19 00:52:40 -0300

    Correciones menores

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Obispado de San Nicolas</title>
+    <title id= "titlesin">Obispado de San Nicolas</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <link rel="icon" type="image/png" href="<?php bloginfo(template_url);?>/img/iconos/circuloRadio.png">
     <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/owl.carousel.min.css">
--- single.php
+++ single.php
@@ -3,14 +3,19 @@
   ?>
   <body>
 
-  <header style="background: url('<?php bloginfo("template_url") ?>/img/portada.jpg') no-repeat center center; background-size:cover;">
-    <p style="color:black;">Obispado de San Nicolas de los Arroyos</p>
-  </header><?php
+  <header id="headersin" style="background: url('<?php bloginfo("template_url") ?>/img/portada.jpg') no-repeat center center; background-size:cover;">
+    <p id="portadin" style="color:black;">Obispado de San Nicolas de los Arroyos</p>
+  </header>
+  <?php
   include "menu.php";
   include 'searchbar.php';
 
   while ( have_posts() ) : the_post();
   ?>
+  <script type="text/javascript">
+    document.getElementById("titlesin").innerHTML="<?php the_title() ?>";
+  </script>
+
   <div class="contenedorSingle">
 
     <h1 class="tituloSingle"><?php the_title(); ?></h1>
@@ -21,17 +26,57 @@
         <a href="?article2pdf=1"><img src="<?php bloginfo(template_url);?>/img/iconos/pdf-icon.png" alt="">PDF version</a>
       </div>
     <?php
-    }
+    }else if (in_category('noticias') ){
+    ?>
+      <script type="text/javascript">
+
+          var x = document.getElementById("headersin");
+          if (document.body.clientWidth<500){
+              x.style.backgroundImage="url(../wp-content/themes/obispadosannicolas/img/celunoticias.jpg)";
+            }else{
+              x.style.backgroundImage="url(../wp-content/themes/obispadosannicolas/img/noticias.jpg)";
+            }
+          
+          var y = document.getElementById("portadin");
+          y.innerHTML="Noticias";
+          y.style.fontFamily="verdana,sans-serif";
+          y.style.color="#fff";
+          y.style.textDecoration="none";
+          y.style.borderBottom= "8px solid #fff";
+          y.style.width="60%";
+
+      </script>
+    <?php
+    }else if (in_category('obispos-anteriores') || in_category('emerito')){
+      ?>
+        <script type="text/javascript">
+
+            var x = document.getElementById("headersin");
+            if (document.body.clientWidth<500){
+              x.style.backgroundImage="url(../wp-content/themes/obispadosannicolas/img/celu2obisposAnteriores.jpg)";
+            }else{
+              x.style.backgroundImage="url(../wp-content/themes/obispadosannicolas/img/2obisposAnteriores.jpg)";
+            }
+            
+            var y = document.getElementById("portadin");
+            y.innerHTML="Obispos Anteriores";
+            y.style.color="#fff";
+
+        </script>
+      <?php
+      } 
     ?>
     </div>
     <?php
   endwhile; // End of the loop.
-  // $categorias = wp_get_post_categories(the_ID());
-  ?><div class="fb-comments" data-href="
+  ?>
+  <h2 class="pieDeComentario">Dejanos tu comentario</h2>
+  <div class="fb-comments" data-href="
   <?php $host= $_SERVER['HTTP_HOST'];
   $url= $_SERVER['REQUEST_URI'];
   echo 'http://' . $host . $url;
   ?>
   " data-numposts="5" colorscheme="dark"></div>
+
   <?php get_footer();
 ?>
