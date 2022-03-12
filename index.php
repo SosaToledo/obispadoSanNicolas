e7a5bfe25d2c94b79af23fb71a55a798d1b962ad
lucho sosa <luchososa1990@gmail.com>
2022-03-12 19:24:17 -0300

    Se ordena la estrucura y se le agrega comentarios TODO

--- index.php
+++ index.php
@@ -1,39 +1,37 @@
+<!-- TODO: revisar error -->
 <?php get_header(); ?>
 <body id="main" onscroll="detect()">
-  <!-- <?php if ( is_active_sidebar( 'home_right_1' ) ) : ?>
-  	<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
-  		<?php dynamic_sidebar( 'home_right_1' ); ?>
-  	</div>
-  <?php endif; ?> -->
-<style media="screen">
-@media screen and (max-width:500px) {
-  header{
-    background-image: url('<?php bloginfo("template_url") ?>/img/celuportada.jpg')!important;
+  <style media="screen">
+  @media screen and (max-width:500px) {
+    header{
+      /* TODO: revisar error */
+      background-image: url('<?php bloginfo("template_url") ?>/img/celuportada.jpg')!important;
+    }
   }
-}
-</style>
-<header style="border-bottom:2px solid #a29f76;background: url('<?php bloginfo("template_url") ?>/img/portada.jpg') no-repeat center center; background-size:cover;">
-  <p style="color:black;">Obispado de San Nicolas de los Arroyos</p>
-</header>
-<!--  -->
-<?php
-$args = array(
-  'posts_per_page' => 4,
-  'category_name' => 'noticias',
-  'meta_key' => 'post_views',
-  'orderby' => 'meta_value_num',
-  'order' => 'DESC'
-);
-include "menu.php";
-include 'searchbar.php';
+  </style>
+  <!-- TODO: se debe cambiar el header -> head -->
+  <!-- TODO: revisar error -->
+  <header style="border-bottom:2px solid #a29f76;background: url('<?php bloginfo("template_url") ?>/img/portada.jpg') no-repeat center center; background-size:cover;">
+    <p style="color:black;">Obispado de San Nicolas de los Arroyos</p>
+  </header>
+  
+  <?php
+    $args = array(
+      'posts_per_page' => 4,
+      'category_name' => 'noticias',
+      'meta_key' => 'post_views',
+      'orderby' => 'meta_value_num',
+      'order' => 'DESC'
+    );
+    include "menu.php";
+    include 'searchbar.php';
+  ?>
 
-?>
+  <div class="contendorData">
+    <div class="tope_slider"></div>
 
-<div class="contendorData">
-  <div class="tope_slider"></div>
-
-  <div class="owl-carousel carouselPortadas">
-    <div class="slides">
+    <div class="owl-carousel carouselPortadas">
+      <div class="slides">
         <?php
         $popular_post = new WP_Query($args);
         while ($popular_post->have_posts()): $popular_post->the_post();
@@ -51,180 +49,184 @@ include 'searchbar.php';
         endwhile;
         ?>
       </div>
-    <div class="slides">
-      <?php
-      query_posts('category_name=noticias');
-      while (have_posts()): the_post();
-      if(has_post_thumbnail()){
-        ?><a href="<?php the_permalink();?>">
-        <?php  the_post_thumbnail('slidePortada'); ?>
-        </a>
+      <div class="slides">
         <?php
-        }?>
-        <a href="<?php the_permalink();?>"><h1 class="tituloSlide"><?php the_title() ?></h1></a>
-        <a href="<?php the_permalink();?>"><h1 class="tituloTipoSlide">Lo ultimo</h1></a>
-        <?php break;
-      endwhile;
+        query_posts('category_name=noticias');
+        while (have_posts()): the_post();
+        if(has_post_thumbnail()){
+          ?><a href="<?php the_permalink();?>">
+          <?php  the_post_thumbnail('slidePortada'); ?>
+          </a>
+          <?php
+          }?>
+          <a href="<?php the_permalink();?>"><h1 class="tituloSlide"><?php the_title() ?></h1></a>
+          <a href="<?php the_permalink();?>"><h1 class="tituloTipoSlide">Lo ultimo</h1></a>
+          <?php break;
+        endwhile;
+        ?>
+      </div>
+
+      <?php
+        query_posts('category_name=banner');
+        while (have_posts()): the_post();
+        echo '<div class="slide";>';
+        if(has_post_thumbnail()){
+          the_post_thumbnail('slidePortada');}
+          //<h1 class="tituloSlide"><?php the_title(); ?/></h1>
+          echo '</div>';
+        endwhile;
       ?>
-    </div>
-    <?php
-      query_posts('category_name=banner');
-      while (have_posts()): the_post();
-      echo '<div class="slide";>';
-      if(has_post_thumbnail()){
-        the_post_thumbnail('slidePortada');}
-        //<h1 class="tituloSlide"><?php the_title(); ?/></h1>
-        echo '</div>';
-      endwhile;
-    ?>
 
-  </div>
-  <script>
-    $('.carouselPortadas').owlCarousel({
-      loop:true,
-      items:4,
-      margin:10,
-      center:true,
-      autoplay:true,
-      autoplayHoverPause:true,
-      autoplayTimeout:3000,
-      dots:true,
-      responsive:{
-        0:{
-          items:1,
-          autoHeight:true
-        },
-        1000:{
-          items:2
+    </div>
+
+    <script>
+      $('.carouselPortadas').owlCarousel({
+        loop:true,
+        items:4,
+        margin:10,
+        center:true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        autoplayTimeout:3000,
+        dots:true,
+        responsive:{
+          0:{
+            items:1,
+            autoHeight:true
+          },
+          1000:{
+            items:2
+          }
         }
-      }
-    })
-  </script>
-  <div class="tituloConBarra">
-    <h1>Nuestra Diocesis</h1>
-    <img class="barraTitulos" src="<?php bloginfo('template_url'); ?>/img/barraObisposAnteriores.png" alt="">
-  </div>
-  <section id="" class="nuestraDiocesis">
-    <div class="diocesisIconos">
-      <a href="<?php bloginfo(url); ?>/historia/">
-        <img src="<?php bloginfo('template_url') ?>/img/iconos/historia.png" alt="">
-        <p>Historia</p>
-      </a>
-    </div>
+      })
+    </script>
 
-    <div class="diocesisIconos">
-      <a href="<?php bloginfo(url); ?>/curia/">
-        <img src="<?php bloginfo('template_url') ?>/img/iconos/curia.png" alt="">
-        <p>Curia</p>
-      </a>
-    </div>
-    <div class="diocesisIconos">
-      <a href="<?php bloginfo(url); ?>/category/calendario-diocesano/">
-        <img src="<?php bloginfo('template_url') ?>/img/iconos/AGENDA.png" alt="">
-        <p>Calendario <br> Diocesano</p>
-      </a>
-    </div>
-  </section>
-
-  <div class="separadorInvertido" style="width:100%;">
-    <img src="<?php bloginfo('template_url') ?>/img/iconos/DIV3.png" alt="">
-  </div>
-
-  <section id="obispoActual" class="obispoActual">
     <div class="tituloConBarra">
-      <h1>Nuestro Obispo</h1>
+      <h1>Nuestra Diocesis</h1>
       <img class="barraTitulos" src="<?php bloginfo('template_url'); ?>/img/barraObisposAnteriores.png" alt="">
     </div>
-    <article id="obispo">
-      <div class="obispoInfoIzq">
-        <div id="IA" class="info">
-          <a href="<?php bloginfo(url)?>/obispo-actual#escudoobispo"><img src="<?php bloginfo('template_url') ?>/img/iconos/ESCUDO.png" alt=""></a>
-          <p>Escudo</p>
-        </div>
-        <div id="IC" class="info">
-          <a href="<?php bloginfo(url)?>/obispo-actual#contenedorObispoActual"><img src="<?php bloginfo('template_url') ?>/img/iconos/BIOGRAFIA.png" alt=""></a>
-          <p>Biografia</p>
-        </div>
-        <div id="IB" class="info">
-          <a href="<?php bloginfo(url)?>/obispo-actual#fotosObispo"><img src="<?php bloginfo('template_url') ?>/img/iconos/FOTOGRAFIAS.png" alt=""></a>
-          <p>Fotografia</p>
-        </div>
-      </div>
-      <div class="obispoImg">
-        <img src="<?php bloginfo('template_url') ?>/img/obispoActual.jpg" alt="">
-        <p></p>
-      </div>
-      <div class="obispoInfoDer">
-        <div id="DA" class="info">
-          <a href="<?php bloginfo(url)?>/category/ultimos-escritos"><img src="<?php bloginfo('template_url') ?>/img/iconos/ULTIMOSESCRITOS.png" alt=""></a>
-          <p>Ultimos<br>escritos</p>
-        </div>
-        <div id="DC" class="info">
-          <a href="<?php bloginfo(url)?>/obispo-actual#videosObispo"><img src="<?php bloginfo('template_url') ?>/img/iconos/VIDEOS.png" alt=""></a>
-          <p>Videos</p>
-        </div>
-        <div id="DB" class="info">
-          <a href="<?php bloginfo(url)?>/category/homilias"><img src="<?php bloginfo('template_url') ?>/img/iconos/HOMILIAS.png" alt=""></a>
-          <p>Homilias</p>
-        </div>
-      </div>
-    </article>
-    <aside class="">
 
-    </aside>
-  </section>
-  <section id="noticias" class="noticias">
+    <section id="" class="nuestraDiocesis">
+      <div class="diocesisIconos">
+        <a href="<?php bloginfo(url); ?>/historia/">
+          <img src="<?php bloginfo('template_url') ?>/img/iconos/historia.png" alt="">
+          <p>Historia</p>
+        </a>
+      </div>
+      <div class="diocesisIconos">
+        <a href="<?php bloginfo(url); ?>/curia/">
+          <img src="<?php bloginfo('template_url') ?>/img/iconos/curia.png" alt="">
+          <p>Curia</p>
+        </a>
+      </div>
+      <div class="diocesisIconos">
+        <a href="<?php bloginfo(url); ?>/category/calendario-diocesano/">
+          <img src="<?php bloginfo('template_url') ?>/img/iconos/AGENDA.png" alt="">
+          <p>Calendario <br> Diocesano</p>
+        </a>
+      </div>
+    </section>
+
     <div class="separadorInvertido" style="width:100%;">
       <img src="<?php bloginfo('template_url') ?>/img/iconos/DIV3.png" alt="">
     </div>
-    <article class="noticiasPrincipales">
-      <div class="noticiasDiv">
-        <a href="<?php bloginfo(url); ?>/category/peregrinaciones/">
-          <img src="<?php bloginfo('template_url') ?>/img/peregrinaciones.jpg" alt="" style="border-radius: 50%;"></a>
-        Peregrinaciones
+
+    <section id="obispoActual" class="obispoActual">
+      <div class="tituloConBarra">
+        <h1>Nuestro Obispo</h1>
+        <img class="barraTitulos" src="<?php bloginfo('template_url'); ?>/img/barraObisposAnteriores.png" alt="">
       </div>
-      <div class="noticiasDiv">
-        <a href="<?php bloginfo(url); ?>/en-construccion/">
-          <img src="<?php bloginfo('template_url') ?>/img/pastoralesCircle.jpg" alt="" style="border-radius: 50%;"></a>
-        Pastorales
+      <article id="obispo">
+        <div class="obispoInfoIzq">
+          <div id="IA" class="info">
+            <a href="<?php bloginfo(url)?>/obispo-actual#escudoobispo"><img src="<?php bloginfo('template_url') ?>/img/iconos/ESCUDO.png" alt=""></a>
+            <p>Escudo</p>
+          </div>
+          <div id="IC" class="info">
+            <a href="<?php bloginfo(url)?>/obispo-actual#contenedorObispoActual"><img src="<?php bloginfo('template_url') ?>/img/iconos/BIOGRAFIA.png" alt=""></a>
+            <p>Biografia</p>
+          </div>
+          <div id="IB" class="info">
+            <a href="<?php bloginfo(url)?>/obispo-actual#fotosObispo"><img src="<?php bloginfo('template_url') ?>/img/iconos/FOTOGRAFIAS.png" alt=""></a>
+            <p>Fotografia</p>
+          </div>
+        </div>
+        <div class="obispoImg">
+          <img src="<?php bloginfo('template_url') ?>/img/obispoActual.jpg" alt="">
+          <p></p>
+        </div>
+        <div class="obispoInfoDer">
+          <div id="DA" class="info">
+            <a href="<?php bloginfo(url)?>/category/ultimos-escritos"><img src="<?php bloginfo('template_url') ?>/img/iconos/ULTIMOSESCRITOS.png" alt=""></a>
+            <p>Ultimos<br>escritos</p>
+          </div>
+          <div id="DC" class="info">
+            <a href="<?php bloginfo(url)?>/obispo-actual#videosObispo"><img src="<?php bloginfo('template_url') ?>/img/iconos/VIDEOS.png" alt=""></a>
+            <p>Videos</p>
+          </div>
+          <div id="DB" class="info">
+            <a href="<?php bloginfo(url)?>/category/homilias"><img src="<?php bloginfo('template_url') ?>/img/iconos/HOMILIAS.png" alt=""></a>
+            <p>Homilias</p>
+          </div>
+        </div>
+      </article>
+    </section>
+
+    <section id="noticias" class="noticias">
+      <div class="separadorInvertido" style="width:100%;">
+        <img src="<?php bloginfo('template_url') ?>/img/iconos/DIV3.png" alt="">
       </div>
-    </article>
+      <article class="noticiasPrincipales">
+        <div class="noticiasDiv">
+          <a href="<?php bloginfo(url); ?>/category/peregrinaciones/">
+            <img src="<?php bloginfo('template_url') ?>/img/peregrinaciones.jpg" alt="" style="border-radius: 50%;"></a>
+          Peregrinaciones
+        </div>
+        <div class="noticiasDiv">
+          <a href="<?php bloginfo(url); ?>/en-construccion/">
+            <img src="<?php bloginfo('template_url') ?>/img/pastoralesCircle.jpg" alt="" style="border-radius: 50%;"></a>
+          Pastorales
+        </div>
+      </article>
+    </section>
+
     <div class="separador" style="width:100%;">
       <img src="<?php bloginfo('template_url') ?>/img/iconos/DIV3.png" alt="">
     </div>
-      <h1 class="noticiasDiv">Actualidad</h1>
-      <div class="owl-carousel carouselProyectos">
-        <?php
 
-          query_posts('category_name=noticias'); // Para excluír se usa el símbolo menos
-            $postNoti = 0;
-            if ( have_posts() ) {
-            	while ( have_posts() ) {
-            		the_post();
-            		if ($postNoti>6) {
-            		  break;
-            		}
-                $postNoti++;
-                ?>
-                <div class="slideProyecto">
+    <h1 class="noticiasDiv">Actualidad</h1>
+    <div class="owl-carousel carouselProyectos">
+      <?php
+
+        query_posts('category_name=noticias'); // Para excluír se usa el símbolo menos
+          $postNoti = 0;
+          if ( have_posts() ) {
+            while ( have_posts() ) {
+              the_post();
+              if ($postNoti>6) {
+                break;
+              }
+              $postNoti++;
+              ?>
+              <div class="slideProyecto">
+              <?php
+              if(has_post_thumbnail()){
+                ?><a href="<?php the_permalink();?>">
+                <?php  the_post_thumbnail('slideProyecto'); ?>
+                </a>
                 <?php
-                if(has_post_thumbnail()){
-                  ?><a href="<?php the_permalink();?>">
-                  <?php  the_post_thumbnail('slideProyecto'); ?>
-                  </a>
-                  <?php
-                }
-                ?><a href="<?php the_permalink();?>"><h1 class="slideProyectoTitulo"><?php the_title() ?></h1></a>
-                </div>
-                <?php
-            		//
-            	} // end while
-            } // end if
-        ?>
+              }
+              ?><a href="<?php the_permalink();?>"><h1 class="slideProyectoTitulo"><?php the_title() ?></h1></a>
+              </div>
+              <?php
+              //
+            } // end while
+          } // end if
+      ?>
 
-      </div>
+    </div>
 
-      <script>
+    <script>
       $('.carouselProyectos').owlCarousel({
         loop:true,
         items:3,
@@ -251,26 +253,26 @@ include 'searchbar.php';
         }
 
       })
-      </script>
-  <footer id="ubicacion">
-    <!-- 900 200 -->
-    <div class="contenedorFooter">
-      <h1>Contacto</h1>
-      <p class="detalleFooter">Obispado de San Nicolas de los Arroyos </p>
-      <p>Bartolomé Mitre 84 - San Nicolas </p>
-      <p>prensaobispadosn@gmail.com </p>
-      <p>Tel. y Fax: (0336) 4422364 / 4429529</p>
+    </script>
+
+    <footer id="ubicacion">
+      <!-- 900 200 -->
+      <div class="contenedorFooter">
+        <h1>Contacto</h1>
+        <p class="detalleFooter">Obispado de San Nicolas de los Arroyos </p>
+        <p>Bartolomé Mitre 84 - San Nicolas </p>
+        <p>prensaobispadosn@gmail.com </p>
+        <p>Tel. y Fax: (0336) 4422364 / 4429529</p>
+      </div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13334.551723278633!2d-60.216407!3d-33.32828!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd24b7cfb43c9fc66!2sObispado+de+San+Nicolas!5e0!3m2!1ses-419!2sar!4v1505959843793" width="900" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </footer>
   </div>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13334.551723278633!2d-60.216407!3d-33.32828!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd24b7cfb43c9fc66!2sObispado+de+San+Nicolas!5e0!3m2!1ses-419!2sar!4v1505959843793" width="900" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
-  </footer>
-</div>
 
-<div class="separador" style="width:100%;">
-  <img src="<?php bloginfo('template_url') ?>/img/iconos/DIV3.png" alt="">
-</div>
+  <div class="separador" style="width:100%;">
+  <!-- TODO: revisar error -->
+    <img src="<?php bloginfo('template_url') ?>/img/iconos/DIV3.png" alt="">
+  </div>
 
-
-<?php get_footer(); 
-include 'despedida.php';?>
-
-<body id="main" onscroll="detect()">
+  <!-- TODO: revisar error -->
+  <?php get_footer(); 
+  include 'despedida.php';?>
